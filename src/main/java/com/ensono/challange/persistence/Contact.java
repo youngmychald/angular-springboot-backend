bd35a3237dd4474af7517a5436611a8bd8c86f56
youngmychald <106515662+youngmychald@users.noreply.github.com>
2022-05-30 22:52:03 -0700

    Update Contact.java

--- src/main/java/com/ensono/challange/persistence/Contact.java
+++ src/main/java/com/ensono/challange/persistence/Contact.java
@@ -1,9 +1,5 @@
 package com.ensono.challange.persistence;
 
-//import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-
-
-
 import javax.persistence.*;
 import java.time.LocalDate;
 
@@ -94,53 +90,4 @@ public class Contact {
 	public void setDateOfBirth(LocalDate dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
-	
-//	public Contact() {
-//		
-//	}
-		
-//	public Contact(String firstName, String lastName, String emailId, String netWorth) {
-//		super();
-//		this.firstName = firstName;
-//		this.lastName = lastName;
-//		this.emailId = emailId;
-//		this.netWorth = netWorth;
-//	}
-//	
-//	
-//	public long getId() {
-//		return id;
-//	}
-//	public void setId(long id) {
-//		this.id = id;
-//	}
-//	public String getFirstName() {
-//		return firstName;
-//	}
-//	public void setFirstName(String firstName) {
-//		this.firstName = firstName;
-//	}
-//	public String getLastName() {
-//		return lastName;
-//	}
-//	public void setLastName(String lastName) {
-//		this.lastName = lastName;
-//	}
-//	public String getEmailId() {
-//		return emailId;
-//	}
-//	public void setEmailId(String emailId) {
-//		this.emailId = emailId;
-//	}
-//
-//	public String getNetWorth() {
-//		return netWorth;
-//	}
-//
-//	public void setNetWorth(String netWorth) {
-//		this.netWorth = netWorth;
-//	}
-//	
-	
-
 }
